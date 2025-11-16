35d716911dd9343e6e66973e3134f3e9e3f932f3
Daniel Hast <hast.daniel@protonmail.com>
2025-11-15 23:30:27 -0500

    fix: missing quotation marks

--- rpm-ostree-run.sh
+++ rpm-ostree-run.sh
@@ -12,10 +12,10 @@ fi
 
 if [ -e "${storage_conf}" ]; then
     sudo sed -i --sandbox \
-        -e "s|^graphroot[[:blank:]]*=.*|graphroot = ${storage_path}|" \
+        -e "s|^graphroot[[:blank:]]*=.*|graphroot = \"${storage_path}\"|" \
         /etc/containers/storage.conf
 else
-    printf '[storage]\ngraphroot = %s\n' "${storage_path}" > "${storage_conf}"
+    printf '[storage]\ngraphroot = "%s"\n' "${storage_path}" > "${storage_conf}"
 fi
 
 rpm-ostree "$@"
